ec83d1be2a57f4db331b70fb4d36bcab7cd1c851
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2026-05-27 15:05:28 +0530

    python started

---
+++ indentation.py
@@ -0,0 +1,4 @@
+# if 5 > 2:
+#     print("Five is greater than two!")
+print("Hello there", end=" ")
+print("How are you?")
---
+++ number.py
@@ -0,0 +1,7 @@
+print(58)
+print (5 * 2)
+
+#mix text ad numbers (comment out the line below to see the error)
+print("The answer is", 5 * 2)
+
+"""This is a multi line comment"""
