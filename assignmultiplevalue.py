64ac6bf65bbbb14aa04fcb0583959ee3302dcb34
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2026-06-05 15:24:50 +0530

    variables

---
+++ assignmultiplevalue.py
@@ -0,0 +1,13 @@
+x, y, z = "Orange", "Banana", "Cherry" #multiple variables are assigned to multiple values in one line
+'''print(x)
+print(y)
+print(z)'''
+
+x = y = z = "Orange" #multiple variables are assigned to the same value in one line
+print(x)
+print(y)
+print(z)
+
+fruits = ["Apple", "Banana", "Cherry"]
+a, b, c = fruits #unpacking a list into multiple variables
+print(a)
---
+++ variable.py
@@ -0,0 +1,29 @@
+name = "Shubham" #string literal is assigned to the variable name
+age = 23  #assignment operator the value od 23 is assigned to the vatiable age RHS = LHS
+price = 100.50 #float literal is assigned to the variable price {Decimal number}
+
+#print("My name is", name, "and I am", age, "years old.")
+
+#getting the type of variables
+'''
+print(type(name))
+print(type(age))
+print(type(price))
+'''
+
+
+a = 100
+b = "100"
+
+'''print(type(a))
+print(type(b))'''
+
+#print (a + b) #this will give an error because we cannot add a string and an integer together
+
+x= "25"
+y = "5"
+#print(x+y) #this will concatenate the two strings together and give us 255 instead of 30 because they are both strings and not numbers
+
+name2="""This is a multi line string"""
+print(name2)
+print(type(name2))
