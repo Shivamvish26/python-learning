ed7e3fd5762399398ad04dc97805b6adfba356a2
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2026-06-16 17:55:18 +0530

    learning python

---
+++ strings/concatenationstring.py
@@ -0,0 +1,12 @@
+# Sting Concatenation or combine using the plus operator
+
+a = "Hello"
+b = "World"
+c = a + b
+print (c)
+# HelloWorld
+
+# For space between then use " " in operator
+d = a + " " + b
+print(d)
+# Hello World
---
+++ strings/modifyingsting.py
@@ -0,0 +1,29 @@
+# python has set of built-in-method that you can use on string
+
+# Upper case :- this method return the string in upper case
+
+a = "Hello World"
+print(a.upper())
+# HELLO WORLD
+
+# Lower Case :- this method return the string in lower case
+
+b = "Hello World"
+print(b.lower())
+# hello world
+
+# Remove Whitespaces :- strip method is used to remove the whitespaces form the beginning and end.
+c = " Hello, World "
+print(c.strip())
+# Hello World
+
+# Replace String :- replace() methos is used to replace the other string
+d = "Hello World"
+print(d.replace("H", "J"))
+# Jello World
+
+# Split String :- split() string is used to sub string the fields into another two other strings
+e = "Hello World"
+print(e.split())
+# ['Hello', 'World']
+
